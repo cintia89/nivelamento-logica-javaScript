f3c1ab5b7fbe3c1150a4f4e89fbdf7f54925d318
cintia <cintia.silvadeooliveira@gmail.com>
2022-09-14 16:08:09 -0300

    resolução dos exercicios de fore while

---
+++ exercicios(for,while)/folha4.js
@@ -0,0 +1,77 @@
+// ENQUANTO
+
+//1
+// let count = 0;
+
+// while (count <= 40) {
+//     console.log(count++);
+//     // count = count + 1;
+// }
+
+//2
+// let n1= 0;
+// while (n1 <= 5){
+//     console.log(n1);
+//     n1= n1 + 1;
+// }
+
+//3
+// let m= 0;
+// let n= 10;
+// while(m < n){
+//     console.log(m++);
+// }
+
+//4
+// let m = 5;
+// let n = 30;
+
+// for (i= m; i < n; i++){
+//     if(i % 2 ==1 ){
+//        console.log(i)
+//     }
+// }
+
+//5
+// let m = 4;
+// let n = 14;
+// let count = 0;
+
+// for (i = m ; i < n; i++){
+//     if(i % 2 == 0 ){
+//         count++
+//                console.log(i)
+//             }
+// } console.log(count)
+
+//6
+// let m = 3;
+// let n = 14;
+
+// for(i=m; i< n; i++){
+//     if(i % 3 ===0){
+//         console.log(i)
+//     }
+// }
+
+//7
+let m = 3;
+let n = 14;
+let i = 2;
+
+for ( j = m; j < n; j+=i) {
+    console.log(j);
+}
+
+//8
+// let n = 10;
+// const min= Math.min();
+
+// for (i = 0; i < n; i++) {
+//     console.log(i)
+//     Math.min([i])
+//     console.log(min)
+// }
+
+
+

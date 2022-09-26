86b1ec68ed8b98052445874a9a2ea80eb4a62a27
cintia <cintia.silvadeooliveira@gmail.com>
2022-09-26 15:35:58 -0300

    algumas correções pequenas

--- exercicios(if,else)/folha1.js
+++ exercicios(if,else)/folha1.js
@@ -1,85 +1,85 @@
 // 1
-var a= 1;
-var b= 1;
-var c= 1;
-var soma= a + b + c;
+let a= 1;
+let b= 1;
+let c= 1;
+let soma= a + b + c;
 console.log(soma); 
 
-var d= 2;
-var e= 2;
-var divisão= d / e ;
+let d= 2;
+let e= 2;
+let divisão= d / e ;
 console.log(divisão);
 
-var anoNasc = 1989;
-var anoAtual = 2022;
-var idade = anoAtual - anoNasc;
+let anoNasc = 1989;
+let anoAtual = 2022;
+let idade = anoAtual - anoNasc;
 if(idade > 18){
 console.log("maior de idade");
 }else {console.log("menor de idade");}
 
 // 1.4
-var nota1= 10;
-var nota2= 8;
+let nota1= 10;
+let nota2= 8;
 
-var calculoMedia= nota1 + nota2 / 2;
+let calculoMedia= nota1 + nota2 / 2;
 if(calculoMedia >= 5){
     console.log("Aprovada");
 }else{console.log(reprovada);}
 
 // 2.1 
-var n1 = 1;
-var n2 = 1;
+let n1 = 1;
+let n2 = 1;
 if(n2 == 0){
     console.log("não pode acontecer o calculo");
 }else{console.log(n1/n2);}
 
 // 2.2
-var numerador = 5;
-var denominador= 5;
+let numerador = 5;
+let denominador= 5;
 if(denominador == 0){
     console.log("não pode acontecer o calculo");
 }else{console.log(numerador/denominador);}
 
 // 2.3
-var salario = 5.000;
-var porcentualDe20= 0.20;
-var aumento= salario * porcentualDe20;
-var novoSalario = salario + aumento;
+// let salario = 5000;
+// let porcentualDe20= 0.20;
+// let aumento= salario * porcentualDe20;
+// let novoSalario = salario + aumento;
 
-var porcentualDe10= 0.10;
-var aumento2= salario * porcentualDe10;
-var novoSalario2= salario+ aumento2;
+// let porcentualDe10= 0.10;
+// let aumento2= salario * porcentualDe10;
+// let novoSalario2= salario+ aumento2;
 
 
-if(salario == 2.500){
-   console.log(novoSalario); 
-}if(salario > 2.500){
-    console.log(novoSalario2);
-}
-
-var imposto= 0.2 ;
-var salario= 3000;
+// if(salario == 2500){
+//    console.log(novoSalario); 
+// }if(salario > 2500){
+//     console.log(novoSalario2);
+// }
+// outra forma de fazer lógica
+let imposto= 0.2 ;
+let salario= 3000;
 
 if (salario > 2500){
     imposto= 0.1;
 }
-var aumento=salario * imposto;
-var novoSalario= salario + aumento;
+let aumento=salario * imposto;
+let novoSalario= salario + aumento;
 console.log(novoSalario);
 
 // 2.4
-var notas1 = 4;
-var notas2 = 5;
-var notas3 = 3;
-var notas4 = 2;
-var mediaNotas = (notas1 + notas2 + notas3 + notas4) /4
-console.log(mediaNotas);
+// let notas1 = 4;
+// let notas2 = 5;
+// let notas3 = 3;
+// let notas4 = 2;
+// let mediaNotas = (notas1 + notas2 + notas3 + notas4) /4
+// console.log(mediaNotas);
 
-if(mediaNotas >= 7){
-    console.log("aprovado");
-} if(mediaNotas < 7 && mediaNotas >= 4){
-    console.log("em exame");
-}else{
-    console.log("reprovado");
-}
+// if(mediaNotas >= 7){
+//     console.log("aprovado");
+// } if(mediaNotas < 7 && mediaNotas >= 4){
+//     console.log("em exame");
+// }else{
+//     console.log("reprovado");
+// }
 
--- exercicios(if,else)/folha2.js
+++ exercicios(if,else)/folha2.js
@@ -38,185 +38,208 @@ var descontoPagaAvista = 0.1;
 // forma de pagamento = ?
 var impostoProdutoParcelado2x = preçoProduto;
 var impostoProdutoParcelado4x = 0.05;
-var pagamento = "4 parcelas";
+// var pagamento = "4 parcelas";
 
 
-if (pagamento === "avista") {
+// if (pagamento === "avista") {
+//     console.log(preçoProduto - (preçoProduto * descontoPagaAvista));
+// }
+// if (pagamento === "2 parcelas") {
+//     console.log(preçoProduto);
+// }
+// if (pagamento === "4 parcelas") {
+//     console.log(preçoProduto + (preçoProduto * impostoProdutoParcelado4x));
+// }
+
+// ou switch, case
+
+let FormaDePagamento; // avista,2 parcelas ,4 parcelas
+FormaDePagamento = 'avista';
+
+switch(FormaDePagamento){
+    case'avista':
     console.log(preçoProduto - (preçoProduto * descontoPagaAvista));
-}
-if (pagamento === "2 parcelas") {
+    break;
+
+    case'2 parcelas':
     console.log(preçoProduto);
-}
-if (pagamento === "4 parcelas") {
+    break;
+
+    case'4 parcelas':
     console.log(preçoProduto + (preçoProduto * impostoProdutoParcelado4x));
+    break;
 }
 
-// 7 
-/*Salario + 25%
-mostrar novosalario */
-var salario = 1000;
-var aumento = salario * 1.25;
-// var novo_Salario= salario + aumento;
-console.log(aumento);
+// // 7 
+// /*Salario + 25%
+// mostrar novosalario */
+// var salario = 1000;
+// var aumento = salario * 1.25;
+// // var novo_Salario= salario + aumento;
+// console.log(aumento);
 
-//8
-/* 
-salario=
-aumento2= 15%
-valor aumento
-novo_salario2
-*/
-const salario4 = 1000;
-var valorAumento = salario4 * 0.15;
-console.log(valorAumento);
-const novo_salario2 = valorAumento + salario4;
-console.log(novo_salario2);
+// //8
+// /* 
+// salario=
+// aumento2= 15%
+// valor aumento
+// novo_salario2
+// */
+// const salario4 = 1000;
+// var valorAumento = salario4 * 0.15;
+// console.log(valorAumento);
+// const novo_salario2 = valorAumento + salario4;
+// console.log(novo_salario2);
 
-/*
-9.
-gradificação + 5%
-imposto -7%
-*/
-const salarioBruto = 1000;
-const imposto2 = 0.07;
-const salarioAumento = salario * 1.05;
-console.log(salarioAumento);
-const salarioImposto = salarioAumento * 0.07;
-const salarioFinal = salarioAumento - salarioImposto;
-console.log(salarioFinal);
+// /*
+// 9.
+// gradificação + 5%
+// imposto -7%
+// */
+// const salarioBruto = 1000;
+// const imposto2 = 0.07;
+// const salarioAumento = salario * 1.05;
+// console.log(salarioAumento);
+// const salarioImposto = salarioAumento * 0.07;
+// const salarioFinal = salarioAumento - salarioImposto;
+// console.log(salarioFinal);
 
-/*
-10.
-nome=
-dataNasc= (dia, mes, ano)
-dia viveu ?
-mes 30 dias
-ano 365 dias
-*/
-const nome = "Cíntia";
-const anoNasc = 1989;
-const anoAtual = 2022;
-const mesNasc = 10;
-const mesAtual = 9;
-let idade = anoAtual - anoNasc;
+// /*
+// 10.
+// nome=
+// dataNasc= (dia, mes, ano)
+// dia viveu ?
+// mes 30 dias
+// ano 365 dias
+// */
+// const nome = "Cíntia";
+// const anoNasc = 1989;
+// const anoAtual = 2022;
+// const mesNasc = 10;
+// const mesAtual = 9;
+// let idade = anoAtual - anoNasc;
 
-if (mesNasc > mesAtual) {
-    idade = idade - 1;
-    console.log("não completou mais um ano de vida");
-}
-let mesDias = mesAtual * 30;
+// if (mesNasc > mesAtual) {
+//     idade = idade - 1;
+//     console.log("não completou mais um ano de vida");
+// }
+// let mesDias = mesAtual * 30;
 
-const diasDeVida = idade * 365 + mesDias;
-console.log(nome, diasDeVida);
+// const diasDeVida = idade * 365 + mesDias;
+// console.log(nome, diasDeVida);
 
-/* 11.
-sobrenome=
-nome=
+// /* 11.
+// sobrenome=
+// nome=
 
-*/
+// */
 
-const nomeN = "Cíntia";
-const sobrenome = "Oliveira";
+// const nomeN = "Cíntia";
+// const sobrenome = "Oliveira";
 
-console.log('Sr(a).' + ' ' + nomeN + ' ' + sobrenome + ' ' + 'é uma honra contar com sua presença!')
+// console.log('Sr(a).' + ' ' + nomeN + ' ' + sobrenome + ' ' + 'é uma honra contar com sua presença!')
 
-/*
-12.
-valor catetos ?
-valor hipotenusa
-calculo (h²= c1 ²+ c2 ²)
-*/
+//ou templete string
 
-let c1 = 9;
-let c2 = 12;
-c1 = (9 ** 2);
-c2 = 12 ** 2;
-let catetos = c1 + c2;
-let hipotenusa = catetos ** (1 / 2);
-console.log(hipotenusa);
+console.log(`Sr(a). ${nomeN} ${sobrenome} 
+é uma honra contar com sua presença!`)
+// /*
+// 12.
+// valor catetos ?
+// valor hipotenusa
+// calculo (h²= c1 ²+ c2 ²)
+// */
 
-/*
-12.
-Triangulo;
-base 
-altura 
-area ?
-area= base * altura /2
-*/
-const base = 8;
-const altura = 6;
-let area = base * altura / 2;
-console.log(area);
+// let c1 = 9;
+// let c2 = 12;
+// c1 = (9 ** 2);
+// c2 = 12 ** 2;
+// let catetos = c1 + c2;
+// let hipotenusa = catetos ** (1 / 2);
+// console.log(hipotenusa);
 
-/*
-area do circulo
+// /*
+// 12.
+// Triangulo;
+// base 
+// altura 
+// area ?
+// area= base * altura /2
+// */
+// const base = 8;
+// const altura = 6;
+// let area = base * altura / 2;
+// console.log(area);
 
-*/
-let raio = 3;
-let areaCirculo = 3.14 * 3 ** 2;
-console.log('area do circulo', areaCirculo)
+// /*
+// area do circulo
 
-/*
-14.
-*/
-let numero = 20;
-let quadrado = numero ** 2;
-let cubo = numero ** 3;
-let raizQuadrada = numero ** (1 / 2);
-let raizCubica = numero ** (1 / 3);
-if (numero > 0) {
-    console.log('a', quadrado, 'b', cubo, 'c', parseFloat(raizQuadrada.toFixed(2)), 'd', parseFloat(raizCubica.toFixed(2))) ;
-}
-//16.
+// */
+// let raio = 3;
+// let areaCirculo = 3.14 * 3 ** 2;
+// console.log('area do circulo', areaCirculo)
 
-let numero1= 4;
-let numero2= 2;
+// /*
+// 14.
+// */
+// let numero = 20;
+// let quadrado = numero ** 2;
+// let cubo = numero ** 3;
+// let raizQuadrada = numero ** (1 / 2);
+// let raizCubica = numero ** (1 / 3);
+// if (numero > 0) {
+//     console.log('a', quadrado, 'b', cubo, 'c', parseFloat(raizQuadrada.toFixed(2)), 'd', parseFloat(raizCubica.toFixed(2))) ;
+// }
+// //16.
 
-let resultado= numero1 ** numero2;
-console.log('potencia', resultado);
+// let numero1= 4;
+// let numero2= 2;
 
-/*17.medidasPolegadas= ;240,00012
-medidaJardas= 20/3;6,66667
-medidasMilhas= ;0,003787880681*/
-medidaPés= 50;
-medidasPolegadas= medidaPés*12;
-medidaJardas= medidaPés/3;
-medidasMilhas= medidaPés/ 1760;
-console.log(parseFloat(medidasPolegadas.toFixed(2)), parseFloat(medidaJardas.toFixed(2)), parseFloat(medidasMilhas.toFixed(2)));
+// let resultado= numero1 ** numero2;
+// console.log('potencia', resultado);
 
-let anoNascimento2 = 2021;
-let anoAtual2 = 2022;
+// /*17.medidasPolegadas= ;240,00012
+// medidaJardas= 20/3;6,66667
+// medidasMilhas= ;0,003787880681*/
+// medidaPés= 50;
+// medidasPolegadas= medidaPés*12;
+// medidaJardas= medidaPés/3;
+// medidasMilhas= medidaPés/ 1760;
+// console.log(parseFloat(medidasPolegadas.toFixed(2)), parseFloat(medidaJardas.toFixed(2)), parseFloat(medidasMilhas.toFixed(2)));
 
-let idade2 = anoAtual2 - anoNascimento2;
-let idade2050 = 2050 - anoNascimento2;
-console.log(idade2, idade2050);
+// let anoNascimento2 = 2021;
+// let anoAtual2 = 2022;
 
-//19.
+// let idade2 = anoAtual2 - anoNascimento2;
+// let idade2050 = 2050 - anoNascimento2;
+// console.log(idade2, idade2050);
 
-let preçoFabrica= 20.000;
-let lucroDistribuidor = 0.50;
-let persentualImposto = 0.10;
+// //19.
 
-let lucro = preçoFabrica * lucroDistribuidor;
-let impost = preçoFabrica * persentualImposto;
-let preçoFinal = lucro + impost +preçoFabrica;
-console.log(lucro, impost, preçoFinal);
+// let preçoFabrica= 20.000;
+// let lucroDistribuidor = 0.50;
+// let persentualImposto = 0.10;
 
-//20.
-let horaTrabalhada= 20;
-let salarioMinimo= 100;
-let valorHora = 1/10 * salarioMinimo;
-let salarioBruto1 = valorHora  * horaTrabalhada;
-let ImpostoSalario = 0.03 * salarioBruto1;
-let salario_Receber = salarioBruto1 - ImpostoSalario;
-console.log(valorHora, salarioBruto1, salario_Receber );
+// let lucro = preçoFabrica * lucroDistribuidor;
+// let impost = preçoFabrica * persentualImposto;
+// let preçoFinal = lucro + impost +preçoFabrica;
+// console.log(lucro, impost, preçoFinal);
+
+// //20.
+// let horaTrabalhada= 20;
+// let salarioMinimo= 100;
+// let valorHora = 1/10 * salarioMinimo;
+// let salarioBruto1 = valorHora  * horaTrabalhada;
+// let ImpostoSalario = 0.03 * salarioBruto1;
+// let salario_Receber = salarioBruto1 - ImpostoSalario;
+// console.log(valorHora, salarioBruto1, salario_Receber );
 
 
-//21.
-let hora= 2;
-let minutos= 60;
-let horaMinuto= hora*60;
-let totalMinutos= minutos + horaMinuto;
-let Minuto_Segundo= totalMinutos * 60 ;
-console.log(horaMinuto, totalMinutos, Minuto_Segundo);
+// //21.
+// let hora= 2;
+// let minutos= 60;
+// let horaMinuto= hora*60;
+// let totalMinutos= minutos + horaMinuto;
+// let Minuto_Segundo= totalMinutos * 60 ;
+// console.log(horaMinuto, totalMinutos, Minuto_Segundo);
 

f6c6e6cfb9e0305d581932aabc84d2ebea314b26
cintia <cintia.silvadeooliveira@gmail.com>
2022-10-19 12:14:35 -0300

    primeiro exercicio de arrays

---
+++ exercicios_arrays/folha5.js
@@ -0,0 +1,11 @@
+/* 1. Crie um algoritmo que leia um vetor de 10 posições e
+depois apresente este vetor em ordem inversa na tela.*/
+// let vetor = [0, 10, 2, 30, 4, 5, 6, 70, 8, 90]
+
+// for (i = 9; i <= vetor.length; i--) {
+//     // console.log(i)     //posição não precisa os conchetes []
+//     console.log(vetor[i])   //valores 
+//     if (i <= 0) {
+//         break
+//     }
+// }
